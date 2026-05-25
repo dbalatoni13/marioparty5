2993b4ef1309a26334f697cbc38909972f718785
Dávid Balatoni <40299962+dbalatoni13@users.noreply.github.com>
2026-05-25 04:01:41 +0200

    Add regex checks for register, volatile, and float globals

--- tools/source_guard.py
+++ tools/source_guard.py
@@ -14,13 +14,26 @@ RAW_POINTER_REGEX = re.compile(
     r"\s*\+\s*0x"
 )
 ASM_REGEX = re.compile(r"\basm\b")
+REGISTER_REGEX = re.compile(r"\bregister\b")
+VOLATILE_REGEX = re.compile(r"\bvolatile\b")
+DECLSPEC_SECTION_REGEX = re.compile(r"__declspec\s*\(\s*section.*")
+FLOAT_GLOBAL_REGEX = re.compile(r"(float|double) lbl_1_rodata.*=\s*\d+.*;")
 
 RAW_POINTER_ERROR = (
     "Access of struct fields through raw pointer math is not allowed. Don't try to rewrite it in a "
     "hacky way just to get it to compile. You must use proper field access or virtual function calls"
 )
 ASM_ERROR = "Inline asm is prohibited."
-
+REGISTER_ERROR = (
+    "The register keyword is prohibited, the devs didn't use it. To fix regalloc, make sure that "
+    "variables are declared in a register-descending order, var_r31, var_r30, var_r29, etc., same for float variables."
+)
+VOLATILE_ERROR = "The volatile keyword is prohibied, the devs didn't use it."
+DECLSPEC_SECTION_ERROR = (
+    "You aren't allowed to use declspec to force globals into a specific data section. "
+    "Figure out the proper C/C++ way, for example applying the const modifier for rodata."
+)
+FLOAT_GLOBAL_ERROR = "You must plug float constants directly into functions."
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
@@ -40,6 +53,14 @@ def check_file(source_file: Path) -> list[tuple[int, str]]:
             violations.append((line_number, RAW_POINTER_ERROR))
         if ASM_REGEX.search(line):
             violations.append((line_number, ASM_ERROR))
+        if REGISTER_REGEX.search(line):
+            violations.append((line_number, REGISTER_ERROR))
+        if VOLATILE_REGEX.search(line):
+            violations.append((line_number, VOLATILE_ERROR))
+        if DECLSPEC_SECTION_REGEX.search(line):
+            violations.append((line_number, DECLSPEC_SECTION_ERROR))
+        if FLOAT_GLOBAL_REGEX.search(line):
+            violations.append((line_number, FLOAT_GLOBAL_ERROR))
 
     return violations
 
